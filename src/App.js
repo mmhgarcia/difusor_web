26b1cf41cafe0225b5c7a166a42673709ac19b7d
Alberto G <mmhgarcia@users.noreply.github.com>
2022-03-17 13:59:32 -0400

    Funciona al pelo

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Primer deploy con React de Alberto. :-
+          Funciona al pelo !!
         </p>        
       </header>
     </div>
