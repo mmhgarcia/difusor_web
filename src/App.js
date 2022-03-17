1f6f9dc281c67b1db9eb49f7d700789d331448e8
Alberto G <mmhgarcia@users.noreply.github.com>
2022-03-17 14:13:22 -0400

    Se añade * a h1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Difusor Web</h1>
+        <h1>Difusor Web.</h1>
         <p>
           Deploy con React de Alberto. :-          
         </p>
