a90c1371292ec047c5be721a1a38751fcec6cba0
Alberto G <mmhgarcia@users.noreply.github.com>
2022-03-17 17:49:12 -0400

    Nuevo mensaje.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Difusor Web.</h1>
+        <h1>Esta App es Difusor Web.</h1>
       </header>
     </div>
   );
