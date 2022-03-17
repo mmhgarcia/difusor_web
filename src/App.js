2f29b6c00c5d6afac8e2fc5b921e3bdd4bdff3f6
Alberto G <mmhgarcia@users.noreply.github.com>
2022-03-17 14:22:11 -0400

    Cambio

--- src/App.js
+++ src/App.js
@@ -5,9 +5,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Difusor Web.</h1>
-        <p>
-          Deploy con React de Alberto. :-          
-        </p>       
       </header>
     </div>
   );
