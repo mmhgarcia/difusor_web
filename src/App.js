e71420ff06728d9162452ab37fffd6f5dec97fac
Alberto G <mmhgarcia@users.noreply.github.com>
2022-03-19 11:35:03 -0400

    Se quitan objetos no usados.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 //import logo from './logo.svg';
 import './App.css';
-import Car from './common/components/Car/Car';
-import Truck from './common/components/Truck/Truck';
-import Calc from './common/components/calc/calc';
+//import Car from './common/components/Car/Car';
+////import Truck from './common/components/Truck/Truck';
+//import Calc from './common/components/calc/calc';
 import Ui from './common/components/julia/ui';
 
 function App() {
