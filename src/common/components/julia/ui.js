3ab08e183df60ac2dde10966dffe4bab49c36847
Alberto G <mmhgarcia@users.noreply.github.com>
2022-03-20 11:42:34 -0400

    cambio 1142

--- src/common/components/julia/ui.js
+++ src/common/components/julia/ui.js
@@ -16,6 +16,7 @@ function ajusteVerticalCover(){
       // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
       let vh = window.innerHeight * 0.01;
 
+      
       // Set the value in the --vh custom property to the root of the document
       document.documentElement.style.setProperty('--vh', `${vh}px`);
 
