484194a7c3eec97dc357e4878557fb2156480c06
Alberto G <mmhgarcia@users.noreply.github.com>
2022-03-20 11:09:36 -0400

    Testing

--- src/common/components/julia/ui.js
+++ src/common/components/julia/ui.js
@@ -1,47 +1,22 @@
 import styles from './ui.module.css';
 import imagen from '../../../assets/playa.jpeg';
 
+function test(){
+  alert("*** TESTING ***");
+}
+
 const Ui = () =>{
   
-    let ua = navigator.userAgent || navigator.vendor || window.opera || window.chrome;
-  
-    let isInstagram = (ua.indexOf('Instagram') > -1) ? true : false;
-
-    if (document.documentElement.classList) {
-
-      if (isInstagram) {
-
-        alert("desde instagram");
-
-        //falta validar que sea desde un mobile *******
-
-        // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-        let vh = window.innerHeight * 0.01;
-
-        // Set the value in the --vh custom property to the root of the document
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-        //OK
-        document.getElementsByClassName("imagen")[0].style.border = "10px solid red";
-
-        //prueba
-        document.getElementsByClassName("imagen")[0].style.marginTop = 5 + "vh";
-
-        //imagen.classList.add("instagram");  
-
-      }
-  
-    }
-    
   return(    
     
     <>
-
       <div className={styles.padre}>
 
         <div className={styles.imagen}>
-          <img src={imagen} alt="" className="imagen"/>
+          <img src={imagen} alt="" className="imagen"/>  
         </div>
+
+        {test()}
 
       </div>
       
