8db7a255d92ca63cad262e522aa43716cf10cf6a
Alberto G <mmhgarcia@users.noreply.github.com>
2022-03-20 10:03:05 -0400

    Haciendo ajuste a imagen

--- src/common/components/julia/ui.js
+++ src/common/components/julia/ui.js
@@ -1,25 +1,77 @@
 import styles from './ui.module.css';
 import imagen from '../../../assets/playa.jpeg';
 
+function adjust(){
+
+  var ua = navigator.userAgent || navigator.vendor || window.opera || window.chrome;
+  //console.log("ua ok");
+
+  var isInstagram = (ua.indexOf('Instagram') > -1) ? true : false;
+  //console.log("isInstagram", isInstagram);
+
+  if (document.documentElement.classList) {
+
+    if (isInstagram) {
+
+      // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+      let vh = window.innerHeight * 0.01;
+
+      // Set the value in the --vh custom property to the root of the document
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+      //OK
+      document.getElementsByClassName("imagen")[0].style.border = "10px solid red";
+
+      //prueba
+      document.getElementsByClassName("imagen")[0].style.marginTop = 5 + "vh";
+
+      //imagen.classList.add("instagram");  
+
+    }
+
+  }
+
+}
+
+
+function TopMenu(){
+  
+  return (
+    <div id="host">
+      TOP MENU
+    </div>
+  );
+}
+
+
+function Pie() {
+
+  adjust();
+
+  return (
+    <div className={styles.pie}>
+      Pie*
+    </div> 
+  );
+}
+
+
 const Ui = () =>{
-  return(
+  
+  return(    
     <>
-    
+
       <div className={styles.padre}>
 
         <div className={styles.menu}>
-          <span id="host">HOST</span>
+          <TopMenu />          
         </div>
 
         <div className={styles.imagen}>
-          <img src={imagen} alt="" />
+          <img src={imagen} alt="" className="imagen"/>
         </div>
-        
 
-        <div className={styles.pie}>
-          Pie
-        </div>        
-
+        <Pie />
 
       </div>
       
