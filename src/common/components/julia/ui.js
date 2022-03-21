477537ae133c7632724f83f74fcdcdcdcbddc0ce
Alberto G <mmhgarcia@users.noreply.github.com>
2022-03-21 07:46:44 -0400

    0746

--- src/common/components/julia/ui.js
+++ src/common/components/julia/ui.js
@@ -4,44 +4,59 @@ import styles from './ui.module.css';
 //let UrlImg = 'https://www.juliamicelipitta.com/imagen/CoverJulia.jpg';
 let urlimagen = 'https://www.juliamicelipitta.com/imagen/CoverJuliaMovil.png';
 
-function fromInstagram(){
+function isInstagramMobile(){
   
   let ua = navigator.userAgent || navigator.vendor || window.opera || window.chrome;
 
   let isInstagram = (ua.indexOf('Instagram') > -1) ? true : false;
 
+  let screenWidth = window.screen.width;
+
+  let isMobil = (screenWidth < 801) ? true : false;
+
+  let _isInstagramMobile = false;
+
   if (document.documentElement.classList) {
 
     if (isInstagram) {
 
-      //falta validar que sea desde un mobile *******
+      if(isMobil){
 
-      // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-      //let vh = window.innerHeight * 0.01;
+        _isInstagramMobile = true;
 
-
-      // Set the value in the --vh custom property to the root of the document
-      //document.documentElement.style.setProperty('--vh', `${vh}px`);
+      }
 
     }
 
-    return isInstagram;
+    return _isInstagramMobile;
 
   }
 }
 
 function Ui(){
 
-  let isInstagram = fromInstagram();
+  let _isInstagramMobile = isInstagramMobile();
 
   return(
     <div className={styles.padre}>
-      <div className={styles.imagen} >
-        <img src={urlimagen} alt="" className="clsimagen" style={{ marginTop: isInstagram ? "7vh" : "0vh" }} />
+      <div className={styles.imagen}>        
+        {
+          _isInstagramMobile ? 
+            <div>
+              <img src={urlimagen} alt="" className="clsimagen" style={{ marginTop: "7vh" }} />      
+            </div> 
+          : 
+            <div>
+              <img src={urlimagen} alt="" className="clsimagen" />
+            </div>
+          
+        }
       </div>
     </div>
   )
 
 }
+
+
 
 export default Ui;
