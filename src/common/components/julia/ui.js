9bb2d9ba7b3d5efc558224d8325cbfa743ea2d0a
Alberto G <mmhgarcia@users.noreply.github.com>
2022-03-21 06:17:12 -0400

    0616

--- src/common/components/julia/ui.js
+++ src/common/components/julia/ui.js
@@ -14,22 +14,11 @@ function ajuste(){
       //falta validar que sea desde un mobile *******
 
       // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-      let vh = window.innerHeight * 0.01;
+      //let vh = window.innerHeight * 0.01;
 
 
       // Set the value in the --vh custom property to the root of the document
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-      //prueba
-      //let _image = document.getElementsByClassName("clsimagen")[0];
-
-      //_image.style .margin = "5vh 0 0 0";
-
-      //document.querySelector(".clsimagen").style.setProperty("margin", "50vh 0 0 0");
-
-      //imagen.classList.add("instagram");  
-
-      //styles.instagram.marginTop="70vh";
+      //document.documentElement.style.setProperty('--vh', `${vh}px`);
 
     }
 
@@ -45,7 +34,7 @@ function Ui(){
   return(
     <div className={styles.padre}>
       <div className={styles.imagen} >
-        <img src={imagen} alt="" className="clsimagen" style={{ marginTop: isInstagram ? "8vh" : "0vh" }} />
+        <img src={imagen} alt="" className="clsimagen" style={{ marginTop: isInstagram ? "7vh" : "0vh" }} />
       </div>
     </div>
   )
