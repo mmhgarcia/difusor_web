10d47f78ab7baa7b1da138a8447b0bb804ccc553
Alberto G <mmhgarcia@users.noreply.github.com>
2022-03-21 21:27:38 -0400

    2127

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import './App.css';
 import Car from './common/components/Car/Car';
 ////import Truck from './common/components/Truck/Truck';
-//import Calc from './common/components/calc/calc';
+import Calc from './common/components/calc/calc';
 //import Ui from './common/components/julia/ui';
 
 function App() {
   return (
     <div className="App">
       <Car />
+      <Calc />
     </div>
   );
 }
