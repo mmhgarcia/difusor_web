8f8ef9b5bc34d4a8298cb5d75287630d9918fef9
Alberto G <mmhgarcia@users.noreply.github.com>
2022-03-21 20:29:44 -0400

    2103

--- src/common/components/Car/Car.js
+++ src/common/components/Car/Car.js
@@ -1,7 +1,12 @@
 import styles from './car.module.css';
 
+function getText(){
+  let text = "Soy la funcion Car!";
+  return text;
+}
+
 const Car = () =>{
-  return <div className={styles.setcolor}>Soy la funcion Car!</div>;
+  return <div className={styles.setcolor}>{getText()}</div>;
 }
 
 export default Car;
