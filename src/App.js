eb9e260f915f4639b6335883b5657e1ecff1b5ef
Alberto G <mmhgarcia@users.noreply.github.com>
2022-03-22 19:34:49 -0400

    1934

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 //import logo from './logo.svg';
 import './App.css';
-import Car from './common/components/Car/Car';
+//import Car from './common/components/Car/Car';
 ////import Truck from './common/components/Truck/Truck';
-import Calc from './common/components/calc/calc';
-//import Ui from './common/components/julia/ui';
+//import Calc from './common/components/calc/calc';
+import Ui from './common/components/julia/ui';
 
 function App() {
   return (
     <div className="App">
-      <Car />
-      <Calc />
+      <Ui />
     </div>
   );
 }
--- src/common/components/julia/ui.js
+++ src/common/components/julia/ui.js
@@ -1,62 +1,13 @@
 import styles from './ui.module.css';
-//import imagen from '../../../assets/playa.jpeg';
-
-//let UrlImg = 'https://www.juliamicelipitta.com/imagen/CoverJulia.jpg';
-let urlimagen = 'https://www.juliamicelipitta.com/imagen/CoverJuliaMovil.png';
-
-function isInstagramMobile(){
-  
-  let ua = navigator.userAgent || navigator.vendor || window.opera || window.chrome;
-
-  let isInstagram = (ua.indexOf('Instagram') > -1) ? true : false;
-
-  let screenWidth = window.screen.width;
-
-  let isMobil = (screenWidth < 801) ? true : false;
-
-  let _isInstagramMobile = false;
-
-  if (document.documentElement.classList) {
-
-    if (isInstagram) {
-
-      if(isMobil){
-
-        _isInstagramMobile = true;
-
-      }
-
-    }
-
-    return _isInstagramMobile;
-
-  }
-}
 
 function Ui(){
 
-  let _isInstagramMobile = isInstagramMobile();
-
   return(
-    <div className={styles.padre}>
-      <div className={styles.imagen}>        
-        {
-          _isInstagramMobile ? 
-            <div>
-              <img src={urlimagen} alt="" className="clsimagen" style={{ marginTop: "7vh" }} />      
-            </div> 
-          : 
-            <div>
-              <img src={urlimagen} alt="" className="clsimagen" />
-            </div>
-          
-        }
-      </div>
-    </div>
+    <>
+      <iframe id="myframe" src="https://www.juliamicelipitta.com" className={styles.myframe}></iframe>
+    </>    
   )
 
 }
-
-
 
 export default Ui;
