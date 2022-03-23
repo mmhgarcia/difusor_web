729aab9881efa15bfbc7665eb54672b5bf6c2aeb
Alberto G <mmhgarcia@users.noreply.github.com>
2022-03-22 20:05:23 -0400

    2005

--- src/common/components/julia/ui.js
+++ src/common/components/julia/ui.js
@@ -5,8 +5,9 @@ function Ui(){
   return(
     <>
       <iframe src="https://www.youtube.com/embed/uXWycyeTeCs" 
-              width={1000} 
-              height={500} 
+              marginTop="20vh"
+              width={700} 
+              height={400} 
               title='A youtube video on React hooks'>                
       </iframe>
     </>    
