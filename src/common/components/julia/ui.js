32bb76ca55dc18ab940615e2aeae1272167332ff
Alberto G <mmhgarcia@users.noreply.github.com>
2022-03-23 16:06:48 -0400

    1606

--- src/common/components/julia/ui.js
+++ src/common/components/julia/ui.js
@@ -6,7 +6,7 @@ function Ui(){
 
   return(
     
-    <iframe src="https://www.google.co.ve/maps/place/Santa+Cruz,+Aragua/@10.1795609,-67.5113993,19.5z/data=!4m5!3m4!1s0x8e80226731ac7073:0xc06908203500885!8m2!3d10.1819305!4d-67.5082709?hl=es-419" width={500} height={400}>               
+    <iframe id="myframe" title="AMJAL" src="https://www.google.co.ve/maps/place/Santa+Cruz,+Aragua/@10.1795609,-67.5113993,19.5z/data=!4m5!3m4!1s0x8e80226731ac7073:0xc06908203500885!8m2!3d10.1819305!4d-67.5082709?hl=es-419" width={500} height={400}>               
     </iframe>
         
   )
