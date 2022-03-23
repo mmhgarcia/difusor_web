d039da500f5ae2e81b9948092a5bf6d0dbc15b13
Alberto G <mmhgarcia@users.noreply.github.com>
2022-03-23 15:55:16 -0400

    1555

--- src/common/components/julia/ui.js
+++ src/common/components/julia/ui.js
@@ -1,14 +1,14 @@
+// https://blog.logrocket.com/best-practices-react-iframes/
+
 //import styles from './ui.module.css';
 
 function Ui(){
 
   return(
     <>
-      <iframe src="https://www.youtube.com/embed/uXWycyeTeCs"              
-              width={250} 
-              height={200} 
-              title='A youtube video on React hooks'> 
-            style={{position:relative, top: "300px",  left: "300px" }}               
+      <iframe src="https://www.google.co.ve/maps/place/Santa+Cruz,+Aragua/@10.1795609,-67.5113993,19.5z/data=!4m5!3m4!1s0x8e80226731ac7073:0xc06908203500885!8m2!3d10.1819305!4d-67.5082709?hl=es-419"              
+              width={500} 
+              height={400}               
       </iframe>
     </>    
   )
