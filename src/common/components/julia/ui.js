dc77eee81a341a16193652352d012a99c1968c6b
Alberto G <mmhgarcia@users.noreply.github.com>
2022-03-25 10:58:24 -0400

    1058

--- src/common/components/julia/ui.js
+++ src/common/components/julia/ui.js
@@ -7,8 +7,7 @@ function Ui(){
 
   return(
 
-    <>
-      <h1>Titulo de la Pagina</h1>
+    <>    
       <div className={styles.marco}>
         <iframe title="visor" src="https://www.juliamicelipitta.com" width="100%" height="100%"></iframe>
       </div>
