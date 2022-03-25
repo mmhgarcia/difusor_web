b5b4609cf645438f18033f60d7861ae723259295
Alberto G <mmhgarcia@users.noreply.github.com>
2022-03-25 10:55:02 -0400

    1054

--- src/common/components/julia/ui.js
+++ src/common/components/julia/ui.js
@@ -6,10 +6,13 @@ import styles from './ui.module.css';
 function Ui(){
 
   return(
-    
-    <div className={styles.marco}>
-      <iframe title="visor" src="https://www.juliamicelipitta.com" width="100%" height="100%"></iframe>
-    </div>
+
+    <>
+      <h1>Titulo de la Pagina</h1>
+      <div className={styles.marco}>
+        <iframe title="visor" src="https://www.juliamicelipitta.com" width="100%" height="100%"></iframe>
+      </div>
+    </>
         
   )
 
